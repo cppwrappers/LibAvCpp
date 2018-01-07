4c29f6b86a6f0158322b836907ef623c007051e2
etienne <squawkcpp@gmail.com>
2018-01-07 12:20:29 +0100

    udpate conanfile

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 set -e
-set -x
 
 IMAGE=spielhuus/toolchain
-TAG=`if [ -z "$1" ]; then echo "master"; else echo "$1" ; fi`
+TAG=`if [ -z "$2" ]; then echo "master"; else echo "$2" ; fi`
 PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/sh)
 echo "build libavcpp tag:$TAG, image:$PID"
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
-#$DOCKER_EXEC "cd .build && conan install /repo --build=missing"
+$DOCKER_EXEC "cd .build && conan install /repo --build=missing"
 $DOCKER_EXEC "cmake -H/repo -B/.build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -Dbuild_tests=on -Dbuild_samples=off -Dbuild_documentation=on  -DLIBAVCPP_VERSION=$TAG"
-#$DOCKER_EXEC "cmake --build /.build"
-#$DOCKER_EXEC "cmake --build /.build --target doc"
+$DOCKER_EXEC "cmake --build /.build"
+$DOCKER_EXEC "cmake --build /.build --target doc"
 
 if [ -z "$1" ]; then
         echo not deploying to bintray, set api key to do so.
